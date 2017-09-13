86bafe65682a56d5b5fc033f4552f6f3796025a2
Jonathan Asbury <JonathanAsbury@SharePointShop.net>
2017-09-13 16:22:27 -0500

    Rollback

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
-import { RegularDeliveryPage } from '../pages/regular-delivery/regular-delivery';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -14,8 +13,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   declarations: [
     MyApp,
     HomePage,
-    ListPage,
-    RegularDeliveryPage
+    ListPage
   ],
   imports: [
     BrowserModule,
@@ -25,8 +23,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage,
-    RegularDeliveryPage
+    ListPage
   ],
   providers: [
     StatusBar,
